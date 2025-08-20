2ba532ef7d8a14af049657816ccd776feeca10bf
the-tushar-sonar <mylordtushar@gmail.com>
2025-08-20 13:17:41 +0530

    feat: add cookie-parser and cors middleware, add asyncHandler utils, update server initialization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.error("Server error:", error);
+            process.exit(1);
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:", error);
+    });
 
 /*
 // Approch 1 :
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+import e from "express";
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+            next(error)
+        );
+    };
+};
+
+export { asyncHandler };
